6b4356af99c1e882fbd423b17b01318d8dae96e2
thangdx-1076 <dang.xuan.thang@sun-asterisk.com>
2025-03-09 17:20:05 +0700

    feat: fix filter pipe

--- src/shared/dto/default-exception.dto.ts
+++ src/shared/dto/default-exception.dto.ts
@@ -4,5 +4,5 @@ import { ErrorMessage } from "../constants/error-message.constant";
 export class DefaultExceptionDto {
   statusCode: HttpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
 
-  message?: string = ErrorMessage[HttpStatus.INTERNAL_SERVER_ERROR];
+  message?: string | object = ErrorMessage[HttpStatus.INTERNAL_SERVER_ERROR];
 }
--- src/shared/filter/external-exception.filter.ts
+++ src/shared/filter/external-exception.filter.ts
@@ -8,6 +8,12 @@ import {
 import { Response } from "express";
 import { DefaultExceptionDto } from "../dto/default-exception.dto";
 
+interface HttpExceptionResponse {
+  statusCode: number;
+  message: unknown;
+  error: string;
+}
+
 @Catch(HttpException)
 export class ExternalExceptionFilter implements ExceptionFilter {
   private readonly logger = new Logger(ExternalExceptionFilter.name);
@@ -19,11 +25,15 @@ export class ExternalExceptionFilter implements ExceptionFilter {
     const defaultExceptionDto = new DefaultExceptionDto();
 
     if (exception instanceof HttpException) {
+      const errorResponse = exception.getResponse() as HttpExceptionResponse;
+
+      const errorMessage = errorResponse.message || exception.message;
+
       defaultExceptionDto.statusCode = exception.getStatus();
-      defaultExceptionDto.message = exception.message;
+      defaultExceptionDto.message = errorMessage;
     }
 
-    const logMessage = `${exception.constructor.name} occurred at ${new Date().toISOString()} - Status: ${defaultExceptionDto.statusCode}, Message: ${defaultExceptionDto.message}`;
+    const logMessage = `${exception.constructor.name} occurred at ${new Date().toISOString()} - Status: ${defaultExceptionDto.statusCode}, Message: ${JSON.stringify(defaultExceptionDto.message)}`;
 
     this.logger.error(logMessage);
     // this.logger.error([logMessage, exception.stack]);
